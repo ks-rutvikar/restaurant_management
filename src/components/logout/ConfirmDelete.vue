365996b10793d40c58044f85367a11cb38f9f6b1
Rutvika Radadiya <rutvika.radadiya@kalkani.in>
2025-03-21 15:34:38 +0530

    Logout Pages

---
+++ src/components/logout/ConfirmDelete.vue
@@ -0,0 +1,50 @@
+<template>
+  <div
+    v-if="show"
+    class="fixed inset-0 bg-gray-100 bg-opacity-50 flex justify-center items-center z-50 transition-opacity duration-300"
+    @click.self="close"
+  >
+    <div
+      class="bg-white p-6 rounded-md shadow-lg w-full max-w-md sm:max-w-lg lg:max-w-xl transform transition-transform duration-300 ease-out"
+    >
+      <h3 class="text-lg font-bold mb-4 text-gray-700 text-center">
+        {{ message || "Are you sure you want to delete this item?" }}
+      </h3>
+      <div class="flex justify-between mt-5">
+        <BaseButton
+          label="Delete"
+          intent="danger"
+          @click="confirm"
+          class="w-28 sm:w-32"
+        />
+        <BaseButton
+          label="Cancel"
+          intent="success"
+          @click="close"
+          class="w-28 sm:w-32"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+  
+  <script setup>
+import { defineProps, defineEmits } from "vue";
+import BaseButton from "../ui/BaseButton.vue";
+
+const props = defineProps({
+  show: Boolean, // Controls modal visibility from parent
+  message: {
+    type: String,
+    default: "Are you sure you want to delete this item?",
+  },
+});
+
+const emit = defineEmits(["confirm", "close"]);
+
+const confirm = () => emit("confirm");
+const close = () => emit("close");
+</script>
+  
+  <style scoped></style>
+  
---
+++ src/components/logout/Logout.vue
@@ -0,0 +1,44 @@
+<template>
+  <!-- Logout Confirmation Popup -->
+  <div
+    v-if="isVisible"
+    class="fixed inset-0 flex items-center justify-center bg-gray-100 bg-opacity-50 z-50 p-4"
+  >
+    <div class="bg-white rounded-lg p-6 w-full max-w-sm shadow-2xl">
+      <h2 class="text-xl font-semibold text-center mb-4 text-gray-900">
+        Are you sure you want to logout?
+      </h2>
+      <div class="flex justify-between gap-4">
+        <!-- Cancel Button -->
+        <BaseButton
+          label="Cancel"
+          intent="inverse"
+          @click="onCancel"
+          class="w-1/2 bg-gray-200 hover:bg-gray-300 text-gray-800 rounded-md py-2"
+        />
+
+        <!-- Logout Button -->
+        <BaseButton
+          label="Logout"
+          intent="danger"
+          @click="onConfirm"
+          class="w-1/2 bg-red-500 hover:bg-red-600 text-white rounded-md py-2"
+        />
+      </div>
+    </div>
+  </div>
+</template>
+
+<script setup>
+import { defineProps } from "vue";
+import BaseButton from "@/components/ui/BaseButton.vue";
+import { useAuthStore } from "@/stores/authStore";
+
+const authStore = useAuthStore();
+
+const props = defineProps({
+  isVisible: Boolean,
+  onCancel: Function,
+  onConfirm: Function,
+});
+</script>
